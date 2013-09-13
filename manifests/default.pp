6193064b13649d0411c7c89bfc416bac61c77f20
digital006 <michael.digital.bell@googlemail.com>
2013-09-13 11:39:47 +0100

    Removed file ownsership which screws up Linux, needs testing on osx now

--- manifests/default.pp
+++ manifests/default.pp
@@ -2,7 +2,7 @@ import 'devsites.pp'
 
 group { 'puppet': ensure => present }
 Exec { path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ] }
-File { owner => 0, group => 0, mode => 0644 }
+#File { owner => 0, group => 0, mode => 0644 }
 
 class {'apt':
   always_apt_update => true,

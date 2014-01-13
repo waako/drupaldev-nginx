a195221b73581f1d9a322bf96fbd51b2ff1a7dc0
Mike Bell <hello@mikebell.io>
2014-01-13 11:28:25 +0000

    Updated drush to 6.2.0

--- manifests/default.pp
+++ manifests/default.pp
@@ -82,7 +82,7 @@ class { '::mysql::server':
   root_password => 'drupaldev'
 }
 
-php::pear::module { 'drush-6.0.0RC4':
+php::pear::module { 'drush-6.2.0':
   repository  => 'pear.drush.org',
   use_package => 'no',
 }
